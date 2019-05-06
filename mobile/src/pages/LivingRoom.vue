4cb355047182913acbd9c3f9e7fb5ad58f00119f
ttosi <ttosi519@gmail.com>
2019-05-05 19:04:12 -0700

    mobile button and events!

--- mobile/src/pages/LivingRoom.vue
+++ mobile/src/pages/LivingRoom.vue
@@ -1,12 +1,31 @@
 <template>
-  <q-page padding>Living Room</q-page>
+  <div>
+    <q-page padding>
+      Living Room
+      <q-btn @click="pickColor('red')" color="red" class="full-width" label="Ceiling pixels red"/>
+      <q-btn @click="pickColor('green')" color="green" text-color="black" class="full-width" label="Ceiling pixels green"/>
+      <q-btn @click="pickColor('blue')" color="blue" class="full-width" label="Ceiling pixels blue"/>
+      <q-btn @click="pickColor('yellow')" color="yellow" text-color="black" class="full-width" label="Ceiling pixels yellow"/>
+      <q-btn @click="pickColor('purple')" color="purple" class="full-width" label="Ceiling pixels purple"/>
+      <q-btn @click="pickColor('white')" color="white" text-color="black" class="full-width" label="Ceiling pixels white"/>
+      <q-btn @click="pickColor('black')" color="black" class="full-width" label="turn Ceiling pixels off"/>
+    </q-page>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "LivingRoom"
+  name: "LivingRoom",
+  methods: {
+    pickColor: (color) => {
+      console.log("color: " + color);
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+  .q-btn {
+    margin: 5px;
+  }
 </style>
